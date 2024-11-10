92d1069ba15d295cc13c346436d10f28ab73c075
Hooriya M. Fareed <parih3080@gmail.com>
2024-11-11 00:44:28 +0500

    first commit

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import about from "../../public/about.png";
 import Image from "next/image";
+
 
 export default function AboutSection() {
   const fadeInUp = {
